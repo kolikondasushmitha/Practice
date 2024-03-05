3ebc9553ac5663895e619aa48df57a03618bde70
kolikondasushmitha <sushmithaanju12@gmail.com>
2024-03-05 22:17:39 +0530

    praticegit

---
+++ git.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class git {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        git obj = new git();
+        obj.add(2,3);
+        
+    }
+    
+}
